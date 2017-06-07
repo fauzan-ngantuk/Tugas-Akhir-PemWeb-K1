9a1e3a02fcafe84811e5268fe1a445b532641754
fauzan-ngantuk <mfauzan.albaihaqi@gmail.com>
2017-06-07 08:17:35 +0700

    add post, put, delete api at user's api

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -15,4 +15,57 @@ class User extends REST_Controller {
 		$user = $this->db->get('user')->result();
 		$this->response($user, 200);
 	}
+
+	public function index_post()
+	{
+		$data = array(
+			'username' => $this->post('username'),
+			'email' => $this->post('email'),
+			'password' => $this->post('password'),
+			'nama' => $this->post('nama'),
+			'level' => $this->post('level'),
+			'foto' => $this->post('foto'),
+			);
+		$insert = $this->db->insert('user', $data);
+		if($insert){
+			$this->response(array('status' => 'succes', 200));
+		}
+		else{
+			$this->response(array('status' => 'fail', 502));
+		}
+	}
+
+	public function index_put()
+	{
+		$user=$this->put('username');
+		$data = array(
+			'username' => $this->put('username'),
+			'email' => $this->put('email'),
+			'password' => $this->put('password'),
+			'nama' => $this->put('nama'),
+			'level' => $this->put('level'),
+			'foto' => $this->put('foto'),
+			);
+		$this->db->where('username', $user);
+		$update = $this->db->update('user', $data);
+		if($update){
+			$this->response(array('status' => 'succes', 200));
+		}
+		else{
+			$this->response(array('status' => 'fail', 502));
+		}
+	}
+
+	public function index_delete()
+	{
+		$user=$this->delete('username');
+		$this->db->where('username', $user);
+		$delete = $this->db->delete('user');
+		if($delete){
+			$this->response(array('status' => 'succes', 200));
+		}
+		else{
+			$this->response(array('status' => 'fail', 502));
+		}
+	}
 }
