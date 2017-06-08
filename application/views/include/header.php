391b50001d1992fdef347b5928112b9f86db0f37
fauzan-ngantuk <mfauzan.albaihaqi@gmail.com>
2017-06-08 18:57:26 +0700

    add favicon

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -6,6 +6,8 @@
       <link type="text/css" rel="stylesheet" href="<?php echo base_url()?>assets2/css/materialize.css"  media="screen,projection"/>
       <link rel="stylesheet" href="<?=base_url()?>assets2/css/table.css">
       <link rel="stylesheet" href="<?=base_url()?>assets2/css/style.css">
+      
+      <link rel="shortcut icon" type="image/png" href="<?=base_url()?>assets2/image/favicon.png"/>
 
       <!--Let browser know website is optimized for mobile-->
       <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"/> -->
