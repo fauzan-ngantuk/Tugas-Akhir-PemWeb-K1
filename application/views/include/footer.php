6ee232697ed20cfe86a45bb88bef89597f5320e1
fauzan-ngantuk <mfauzan.albaihaqi@gmail.com>
2017-06-08 18:53:36 +0700

    enchant header & footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,8 +1,10 @@
 
-<div class="footer-copyright">
+<div class="footer-copyright" style="padding: 30px 0px">
           <div class="container">
-
-            <p align="center">&copy; 2017 Nyekolah.ID Developer</p>
+          	<center>
+          		<img src="<?=base_url()?>assets2/image/logo.png" style="height: 35px; width: auto; margin-top: 12px">
+          	</center>
+            <p align="center" style="color: #999">&copy; 2017 Nyekolah.ID Developer</p>
           </div>
         </div>
   
