0ad8c662d7a6c121b5a5dea504cd8c038245100a
fauzan-ngantuk <mfauzan.albaihaqi@gmail.com>
2017-06-08 19:05:22 +0700

    enchant jumbotron

--- application/views/Jumbotron.php
+++ application/views/Jumbotron.php
@@ -3,10 +3,10 @@
       <div class="container">
         <h1 class="header jarak center white-text" style="font-size: 96px">JOGJA KOTA PELAJAR</h1>
         <div class="row center">
-          <h5 class="header jarak-button col s12 light" style="margin-bottom: 0px;">DATA POKOK PENDIDIKAN DASAR DAN MENENGAH DAERAH ISTIMEWA YOGYAKRATA</h5>
+          <h5 class="header jarak-button col s12 light" style="margin-bottom: 0px; font-size: 18px">DATA POKOK PENDIDIKAN DASAR DAN MENENGAH DAERAH ISTIMEWA YOGYAKRATA</h5>
         </div>
-        <div class="row center">
-          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">SELENGKAPNYA</a>
+        <div class="row center" \>
+          <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1" style="margin-top: 20px">SELENGKAPNYA</a>
         </div>
         <br><br>
 
