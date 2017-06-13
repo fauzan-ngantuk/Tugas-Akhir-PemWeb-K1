01c1991b8bb1adbdc6104aec1e920be34abd9676
fauzan-ngantuk <mfauzan.albaihaqi@gmail.com>
2017-06-14 05:26:24 +0700

    fix directory image on card

--- application/views/berita.php
+++ application/views/berita.php
@@ -30,10 +30,10 @@
           <div class="col s12 m4">
             <div class="card">
               <div class="card-image">
-                <div style="height: 200px; width: 100%; background-image: url(<?php echo $key->userPic; ?>); background-size: 100%"></div>
+                <div style="height: 200px; width: 100%; background-image: url('http://localhost/admin/page/cover-image/<?php echo $key->userPic; ?>'); background-size: 100%"></div>
                 <h5 class="header" style="margin-left: 24px; margin-bottom: 0px; margin-top: 24px; color: #26A69A"><?=$key->judul;?></h5>
               </div>
-              <div class="card-content">
+              <div class="card-content" style="min-height: 137px">
                 <?=$key->deskripsi;?>
               </div>
               <div class="card-action">
